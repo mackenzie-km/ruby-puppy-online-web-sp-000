78e84883f0bb59058e5c6e7019238e94b84f8ec7
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-28 23:18:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,20 @@
 class Dog
   attr_accessor :name 
 @@all = []
+@@names = []
   
 def  self.clear_all
   @@all = []
 end 
 
 def self.all 
-  puts @@all[@name] 
+  puts @@names
 end 
 
 def initialize(name = nil)
   @name = name 
   @@all << self
+  @@names << @name
 end 
 
 end 
